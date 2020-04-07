845197ff4544a6301afaf070d8c492ff7047144b
Md Mamunur Rahman <mamun_131@hotmail.com>
2020-04-07 06:59:08 -0400

    changes

--- scripts/app/app_script.js
+++ scripts/app/app_script.js
@@ -2,18 +2,63 @@ var windowKit = new windowKit({
   account: 34681503,
   //skillId: 12341234 - optional skill ID
 });
+$("#hideSegment").hide();
+
+const commandName = "my-command";
+
+function resetShortcut() {
+  browser.commands.reset(commandName);
+}
+
+document.querySelector("#closeChat").addEventListener("click", resetShortcut);
 
 //connect to LE
 windowKit.connect();
 windowKit.onConnect(function (res) {
   //alert(res);
   $(document).ready(function () {
-    $("#sendButton").click(function () {
-      windowKit.sendMessage($("#textline").val());
+    $("#startChat").click(function () {
+      // window.location.reload(true);
+      setTimeout(function () {
+        // alert("Hello");
+        $("#hideSegment").show();
+        $("#startHere").hide();
+        scrollToBottom();
+        //  windowKit.sendMessage("reset");
+        // windowKit.sendMessage("hi");
+        // $("#chatLines").empty();
+      }, 1000);
+    });
+  });
+  $(document).ready(function () {
+    $("#closeChat").click(function () {
+      $("#chatLines").empty();
+      $("#hideSegment").hide();
+
+      window.location.reload(true);
+
+      //  $("#startHere").show();
     });
   });
 
-   console.log(res);
+  $("#hideSegment").hide();
+
+  $(document).ready(function () {
+    $("#sendButton").click(function () {
+      var line = createLine({
+        by: "You",
+        text: $("#textline").val(),
+        source: "visitor",
+        type: "text",
+      });
+      $("#chatLines").append(line);
+      windowKit.sendMessage($("#textline").val());
+      $("#textline").val("");
+      scrollToBottom();
+    });
+  });
+
+  console.log(res);
   //console.log(JSON.stringify(res.initStack));
   //console.log(res.initStack);
 });
@@ -25,34 +70,55 @@ windowKit.onMessageEvent(function (res) {
   //console.log(res);
 });
 
-//windowKit.sendMessage("Hello World!");
 //when the agent sends a text message
 windowKit.onAgentTextEvent(function (text) {
+  //  if (text === "Session deleted") {
+  //  return;
+  // }
   //append the message's contents to the DOM
-  $("#chatbox").append('<div class="agentText">' + text + "</div>");
+  var line = createLine({
+    by: "Bot",
+    text: text,
+    source: "agent",
+    type: "text",
+  });
+  $("#chatLines").append(line);
   //grab all the agent texts so far
-  var botTexts = document.getElementsByClassName("agentText");
+  var botTexts = document.getElementsByClassName("response");
   //find the last one
   var latestText = botTexts[botTexts.length - 1];
   //scroll the window to the last text. This is used to create a scroll effect in the conversation.
-  $("body, html").animate({ scrollTop: $(latestText).offset().top }, 1000);
-  // alert("mmm");
+  scrollToBottom();
   // console.log("Agent: " + text);
 });
 
-//windowKit.setInteractions(1).sendMessage("hi");
-
 //when the agent sends a rich content message
 windowKit.onAgentRichContentEvent(function (content) {
+  console.log(content.elements[0].text);
   //render the structured content using JsonPollock
+  console.log(content);
+  var line1 = createLine({
+    by: "Bot",
+    text: content.elements[0].text,
+    source: "agent",
+    type: "rttext",
+  });
+
+  $("#chatLines").append(line1);
+  content.elements.splice(0, 1);
   var structuredText = JsonPollock.render(content);
+
+  var line2 = createLine({
+    by: "Bot",
+    text: structuredText,
+    source: "agent",
+    type: "rtext",
+  });
+
   //append the results of the render to the DOM
-  $("#chatbox").append(structuredText);
-  //next three rows create the same scrolling effect as above
-  var botTextsSC = document.getElementsByClassName("lp-json-pollock");
-  var latestSC = botTextsSC[botTextsSC.length - 1];
-  $("body, html").animate({ scrollTop: $(latestSC).offset().top }, 1000);
-  console.log("Agent: ", structuredText);
+  $("#chatLines").append(line2);
+  scrollToBottom();
+  console.log(line2);
   //when a user clicks on a structured content button
   $(".lp-json-pollock-element-button").on("click", function () {
     //grab the text of the button
@@ -60,25 +126,70 @@ windowKit.onAgentRichContentEvent(function (content) {
     //send the text to LE for the bot to process
     windowKit.sendMessage(scText);
     //append the text to the DOM so it shows up as the user's side of the conversation
-    $("#chatbox").append('<div class="consumerText">' + scText + "</div>");
+    var line_button = createLine({
+      by: "You",
+      text: $(this).text(),
+      source: "visitor",
+      type: "text",
+    });
+    $("#chatLines").append(line_button);
     //same scroll effect as above
-    console.log("Agent: " + scText);
-    var consumerTexts = document.getElementsByClassName("consumerText");
-    var latestConsumerText = consumerTexts[consumerTexts.length - 1];
-    $("body, html").animate(
-      { scrollTop: $(latestConsumerText).offset().top },
-      1000
-    );
+    console.log(scText);
+    scrollToBottom();
   });
 });
 
 JsonPollock.registerAction("publishText", (data) => {
-  alert(JSON.stringify(data));
-  alert(data.actionData.text);
-  $("#textline").val(data.actionData.text);
   windowKit.sendMessage(data.actionData.text);
+  var line = createLine({
+    by: "You",
+    text: data.actionData.text,
+    source: "visitor",
+    type: "text",
+  });
+  scrollToBottom();
 });
 
 windowKit.onMessageSent(function (res) {
-  console.log("mmmm" + res);
+  console.log(res);
 });
+
+function scrollToBottom() {
+  $("#chatLines").scrollTop(
+    $("#chatLines")[0].scrollHeight - $("#chatLines")[0].clientHeight
+  );
+}
+
+//Create a chat line
+function createLine(line) {
+  var div = document.createElement("P");
+
+  if (line.source === "visitor") {
+    div.innerHTML =
+      "<img src='/assets/image/you.PNG' style='width: 40px; height: 40px;'/><b>" +
+      line.by +
+      "</b>: ";
+    div.setAttribute("class", "request");
+    div.appendChild(document.createTextNode(line.text));
+  } else {
+    if (line.type === "rtext") {
+      div.append(line.text);
+    } else if (line.type === "rttext") {
+      div.innerHTML =
+        "<img src='/assets/image/boticon.PNG' style='width: 40px; height: 40px;'/><b>" +
+        line.by +
+        "</b>: ";
+      div.setAttribute("class", "response1");
+      div.innerHTML += line.text;
+    } else {
+      div.innerHTML =
+        "<img src='/assets/image/boticon.PNG' style='width: 40px; height: 40px;'/><b>" +
+        line.by +
+        "</b>: ";
+      div.setAttribute("class", "response");
+      div.innerHTML += line.text;
+    }
+  }
+
+  return div;
+}

95623b4db341926859eb2957dca139e088e2b6a3
Md Mamunur Rahman <mamun_131@hotmail.com>
2020-04-24 16:25:07 -0400

    image problem got solved

--- scripts/app/app_script.js
+++ scripts/app/app_script.js
@@ -1,7 +1,9 @@
 var windowKit = new windowKit({
   account: 34681503,
- // skillId: 1933536430,
-  skillId: 1933526730,
+  // skillId: 1933536430,
+  //skillId: 1933526730,
+  skillId: 1912201930,
+
   //skillId: 12341234 - optional skill ID
 });
 
@@ -62,19 +64,18 @@ windowKit.onReady(function (res) {
 
 windowKit.onMessageEvent(function (res) {
   //console.log(res);
- // console.log(res.conversationId);
-  
+  // console.log(res.conversationId);
 });
 
 //when the agent sends a text message
 windowKit.onAgentTextEvent(function (text) {
-  var jwt = text.substring(0, 3); 
-  if (jwt === 'jwt') {
-    console.log(text.substring(4, text.length));
-    sendVariable(text.substring(4, text.length),jwtCallback().jwt);
-    console.log(jwtCallback().jwt);
-   // console.log(windowKit.initStack[0].jwt);
-   // windowKit.sendMessage(jwtCallback().jwt);
+  var jwt = text.substring(0, 3);
+  if (jwt === "jwt") {
+    // console.log(text.substring(4, text.length));
+    sendVariable(text.substring(4, text.length), jwtCallback().jwt);
+    //console.log(jwtCallback().jwt);
+    // console.log(windowKit.initStack[0].jwt);
+    // windowKit.sendMessage(jwtCallback().jwt);
     return;
   }
   //append the message's contents to the DOM
@@ -95,7 +96,7 @@ windowKit.onAgentTextEvent(function (text) {
 
 //when the agent sends a rich content message
 windowKit.onAgentRichContentEvent(function (content) {
-  //console.log(content.elements[0].text);
+  console.log(content);
   //render the structured content using JsonPollock
   var line1 = createLine({
     by: "Bot",
@@ -105,7 +106,7 @@ windowKit.onAgentRichContentEvent(function (content) {
   });
 
   $("#chatLines").append(line1);
-  content.elements.splice(0, 1);
+  // content.elements.splice(0, 1);
   var structuredText = JsonPollock.render(content);
 
   var line2 = createLine({
@@ -151,9 +152,8 @@ JsonPollock.registerAction("publishText", (data) => {
 });
 
 windowKit.onMessageSent(function (res) {
- // console.log(res);
+  // console.log(res);
   //console.log(res.conversationId);
-  
 });
 
 function scrollToBottom() {
@@ -205,43 +205,41 @@ windowKit.onAgentChatState(function (res) {
   }
 });
 
-
-
-
 /////////////////////////////////////////////////////////////////
 
 // Post data to Web View API
-async function postData(url = '', auth, data = {}) {
+async function postData(url = "", auth, data = {}) {
   const response = await fetch(url, {
-    method: 'POST',
-    mode: 'cors',
+    method: "POST",
+    mode: "cors",
     headers: {
-      'Authorization': auth,
-      'Content-Type': 'application/json',
+      Authorization: auth,
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   });
   return await response.json();
-};
+}
 
 // method to sha256 encode string
 async function sha256(message) {
-  const msgBuffer = new TextEncoder('utf-8').encode(message);
-  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+  const msgBuffer = new TextEncoder("utf-8").encode(message);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", msgBuffer);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
-  return hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+  return hashArray.map((b) => ("00" + b.toString(16)).slice(-2)).join("");
 }
 
 // Send Variable
- async function sendVariable(botid,jwt) {
-// Get data from query string
+async function sendVariable(botid, jwt) {
+  // Get data from query string
   const userId = windowKit.participantId;
   const conversationId = Object.keys(windowKit.openConvs)[0];
   const botId = botid;
 
   // use correct domain for your region
-  const domain = 'https://va.bc-intg.liveperson.net/thirdparty-services-0.1/webview';
-  
+  const domain =
+    "https://va.bc-intg.liveperson.net/thirdparty-services-0.1/webview";
+
   // encode auth string
   const authString = `${conversationId} || ${botId}`;
   const auth = await sha256(authString);
@@ -252,28 +250,9 @@ async function sha256(message) {
     userId,
     message: "request successful", // optional
     contextVariables: [
-      {"name": "jwt", "value": jwt},
-      {"name": "color", "value": "ccccccccccccccc"},
-      {"name": "swallow", "value": "ssssssssssss"}
+      { name: "jwt", value: jwt },
+      { name: "color", value: "ccccccccccccccc" },
+      { name: "swallow", value: "ssssssssssss" },
     ],
   });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
